e0e22c8dd10ca44c3361a04df10c1a5ab4fcc879
KIO <kio.watanabe.1005@gmail.com>
2021-11-09 15:14:19 +0900

    test-uocmo
    ためにしアップロード

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Uocmo 自立訓練 / 就労移行支援</title>
+		<link href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+		<script>
+			(function(d) {
+				var config = {
+					kitId: 'jar4asz',
+					scriptTimeout: 3000,
+					async: true
+				},
+				h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+			})(document);
+		</script>
+	</head>
+	<body> 
+		<section id="main-view">
+			<!-- <div class="title">
+				<img src="/assets/img/Uocmo-logo.svg" alt="Uocmoロゴ">
+			</div> -->
+			<!-- <video id="bg-movie" src="/assets/movie/main-bg.mp4" autoplay loop muted> -->
+				<!-- <img src="assets/img/main-bg.png" alt="Placeholder"> -->
+			</video>
+		</section>
+		<section id="intro-message">
+			<div class="container">
+				<div class="colum message">
+						<h3>あなたは今、<br>何をして過ごしたいですか？</h3>
+						<p>
+						皆さんの希望を聞きながら様々なことに取り組んでいきます。<br class="pc">
+						気持ちが安定しなくて、不安やソワソワ・時には怒りの感情になるときに<br class="pc">
+						原因や要因となっていることを考えて対処していくこと。<br class="pc">
+						日常生活の困りごとを整理して解消していくこと。<br class="pc">
+						自宅に一人でいることからちょっと人に会う機会を作っていくこと。<br class="pc">
+						やりたいことがあるけど自分でできるかどうかわからないことを<br class="pc">
+						チャレンジしてみること。<br class="pc">
+						目的に必要なサービスや商品・設備がないかを考えていくこと。<br>
+						<br>
+						様々な目的によって変わる居場所。<br>
+						<br>
+						ここに挙がっていることも一例です。<br class="pc">
+						皆さんの目的から一緒に考えて行きましょう。<br class="pc">
+						</p>
+						<h3>手に入れたいものは、<br>なんですか？</h3>
+				</div>
+				<div class="colum copy">
+					<img src="/assets/img/bland-text.svg" alt="bland-text">
+				</div>
+			</div>
+		</section><!--/#intro-message-->
+		<section id="about">
+			<div class="img-area">
+				<img src="/assets/img/about_main.jpg" alt="main">
+			</div>
+			<div class="container">
+				<h2 class="sub-title">Uocmoとは？</h2>
+				<p class="text">Uocmo（ウォクモ）は兵庫県西宮市で、自立訓練・就労移行支援を行う福祉事業所です。Uocmoとはベトナム語で「夢見る人」という意味。<br> 自分自身の殻を自分自身で破り、それぞれが自分の夢を描いて実現に向かう場所。それがUocmoです。</p>
+			</div>
+			<ul class="slider">
+				<li><img src="/assets/img/uocmo-1.jpg" alt="uocmo-1"></li>
+				<li><img src="/assets/img/uocmo-2.jpg" alt="uocmo-2"></li>
+				<li><img src="/assets/img/uocmo-3.jpg" alt="uocmo-3"></li>
+				<li><img src="/assets/img/about_main.jpg" alt="uocmo-4"></li>
+			</ul>
+		</section><!--/#about-->
+		<section id="point">
+			<div class="container">
+				<h2 class="sub-title">大切にしていること</h2>
+				<p class="sub-title-text">大切にしている３つの取り組み</p>
+				<div class="inner">
+					<div class="colum">
+						<img src="/assets/img/point-01.svg" alt="point-01">
+						<h3>自分で決める</h3>
+						<p>「自分で決める」ということは責任が伴うことでもあります。<br>一方で活動のエネルギーともなります。就職など人生の岐路に立つ時、この小さな「自分で決める」の繰り返しが必ず活きてきます。</p>
+					</div>
+					<div class="colum">
+						<img src="/assets/img/point-02.svg" alt="point-02">
+						<h3>やってみる</h3>
+						<p>やってみる・チャレンジの機会を大切にしています。初めてのこと、ちょっと勇気がいること、あまりやりたくないことでも、小さなチャレンジの繰り返しがやがて大きな目標に向けてのエネルギーとなります。</p>
+					</div>
+					<div class="colum">
+						<img src="/assets/img/point-03.svg" alt="point-03">
+						<h3>振り返る</h3>
+						<p>不安な時、イライラした時、うまく行かなかった時、「なぜ」そうなったのかを一緒に振り返りましょう。繰り返し振り返ることで自分自身を客観的に捉えて対処方法を考えていくことができます。</p>
+					</div>
+				</div>
+			</div>
+		</section><!--/#point-->
+
+
+
+
+
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+		<script src="/assets/js/script.js"></script>
+	</body>
+</html>
